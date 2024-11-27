1aeee490421aa5a0395bdc54b9d2ae1cbb9d6943
Hikaru Nemoto <s23C1105ZV@s.chibakoudai.jp>
2024-11-27 18:40:07 +0900

    binaryのテストを追加した

--- test.bash
+++ test.bash
@@ -7,6 +7,8 @@ ng () {
 
 res=0
 
+#Test of plus command
+#CORRECT INPUT
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
@@ -19,6 +21,29 @@ out=$(echo | ./plus)
 [ "$?" = 1 ]       || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
+#Test of binary command
+#CORRECT INPUT
+out=$(seq 3 | ./binary.py)
+[ "${out}" = 11 ] || ng "$LINENO"
+
+out=$(seq 100 | ./binary.py)
+[ "${out}" = 1100100 ] || ng "$LINENO"
+
+out=$(seq 51 | ./binary.py)
+[ "${out}" = 110011 ] || ng "$LINENO"
+
+#STRANGE INPUT
+out=$(echo あ | ./binary.py)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./binary.py)
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo 0.5 | ./binary.py)
+[ "$?" = 1 ]       || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "$res" = 0 ] && echo OK
 exit $res
-
